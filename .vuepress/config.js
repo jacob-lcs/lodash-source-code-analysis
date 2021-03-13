e4630b53e6f5e5a113558abfe8ddd17bbe7bb852
jacob-lcs <jacob.lcs@qq.com>
2021-03-13 13:25:01 +0800

    docs: add 首页

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -9,7 +9,7 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: "首页", link: "/" },
-      { text: "指南", link: "/guides/" },
+      { text: "源码解析", link: "/analysis/" },
       {
         text: '查看更多',
         ariaLabel: 'see more',
@@ -18,7 +18,6 @@ module.exports = {
           { text: '开心消消乐', link: 'http://schedule.lcs.show/', target:'_blank' }
         ]
       },
-      { text: 'GitHub', link: 'https://github.com/jacob-lcs/lodash-source-code-analysis', target:'_blank' },
     ],
     repo: 'https://github.com/jacob-lcs/lodash-source-code-analysis',
     repoLabel: '查看源码',
---
+++ index.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroImage: https://www.lodashjs.com/img/lodash.png
+heroText: Lodash 源码解析
+tagline: 深入分析源码逻辑，学习编码思维
+actionText: 马上进入 →
+actionLink: /guides/index.md
+---
